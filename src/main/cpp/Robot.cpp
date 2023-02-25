13494e7d8a1c967e93cfc549d1d16d9956ec9840
mimi980 <90112270+mimi980@users.noreply.github.com>
2023-02-25 21:41:53 +0100

    encoder

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -26,55 +26,54 @@ void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit()
 {
-  // m_encoder.Reset();
-  // m_encoder.SetDistancePerPulse(1.0 / 2048.0);
+  m_encoder.Reset();
+  m_encoder.SetDistancePerPulse(1.0 / 2048.0);
 
-  // m_motor.SetInverted(true);
-  // m_motor.SetSmartCurrentLimit(40);
-  // m_motor.EnableVoltageCompensation(12);
-  // m_motor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
-  // frc::SmartDashboard::PutNumber("clamp", 0.0);
+  m_motor.SetInverted(true);
+  m_motor.SetSmartCurrentLimit(40);
+  m_motor.EnableVoltageCompensation(12);
+  m_motor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+  frc::SmartDashboard::PutNumber("clamp", 0.0);
 }
 void Robot::TeleopPeriodic()
 {
   m_compressor.EnableDigital();
+  m_clamp = frc::SmartDashboard::GetNumber("clamp", 0.0);
   // if (m_encoder.GetDistance() < 0)
   // {
   //   m_motor.Set(0.0);
   // }
-  // m_encoderGetDistance = m_encoder.GetDistance();
-  // m_vitesse = (m_encoderGetDistance - m_lastDistance) / 0.02;
-  // // if (m_stick.GetRawButton(1))
-  // // {
-  // //   if (m_encoder.GetDistance() < 3)
-  // //   {
-  // //     m_motor.Set(-1.0);
-  // //   }
-  // //   else
-  // //   {
-  // //     m_motor.Set(0.0);
-  // //   }
-  // // }
-  // // else
-  // // {
-  // //   m_motor.Set(m_stick.GetY());
-  // // }
-  // frc::SmartDashboard::PutNumber("encoder", m_encoder.GetDistance());
-  // frc::SmartDashboard::PutNumber("vitesse", m_vitesse);
-  // frc::SmartDashboard::PutNumber("current", m_motor.GetOutputCurrent());
-  // // frc::SmartDashboard::PutNumber("");
-  // m_clamp = frc::SmartDashboard::GetNumber("clamp", 0.0);
-  // m_lastDistance = m_encoderGetDistance;
-
-  if (m_stick.GetRawButtonPressed(1))
+  m_encoderGetDistance = m_encoder.GetDistance();
+  m_vitesse = (m_encoderGetDistance - m_lastDistance) / 0.02;
+  if (m_stick.GetRawButton(1))
   {
-    m_solenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+    if (m_encoder.GetDistance() < 2)
+    {
+      m_motor.Set(-0.8);
+    }
+    else
+    {
+      m_motor.Set(0.0);
+    }
   }
-
-  if (m_stick.GetRawButtonPressed(2))
+  else
   {
-    m_solenoid.Set(frc::DoubleSolenoid::Value::kForward);
+    m_motor.Set(m_stick.GetY() * m_clamp);
   }
+  frc::SmartDashboard::PutNumber("encoder", m_encoder.GetDistance());
+  frc::SmartDashboard::PutNumber("vitesse", m_vitesse);
+  frc::SmartDashboard::PutNumber("current", m_motor.GetOutputCurrent());
+  m_lastDistance = m_encoderGetDistance;
+
+  // if (m_stick.GetRawButtonPressed(1))
+  // {
+  //   m_solenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+  // }
+
+  // if (m_stick.GetRawButtonPressed(2))
+  // {
+  //   m_solenoid.Set(frc::DoubleSolenoid::Value::kForward);
+  // }
 }
 
 void Robot::DisabledInit() {}
