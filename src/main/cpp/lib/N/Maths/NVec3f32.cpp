e9c60b2e93978d826f2270e43f2baa6c766e35e6
mimi980 <90112270+mimi980@users.noreply.github.com>
2023-03-25 10:15:04 +0100

    ça a l'air bon

--- src/main/cpp/lib/N/Maths/NVec3f32.cpp
+++ src/main/cpp/lib/N/Maths/NVec3f32.cpp
@@ -3,13 +3,14 @@
 // ------------------------------------------------------------------------------------------
 // Author  : Nzt
 // Created : 03/01/2012
-// Updated : 
+// Updated :
 // ------------------------------------------------------------------------------------------
 // Vectors management.
 // ==========================================================================================
 #include "lib/N/NCStandard.h"
 #include "lib/N/Maths/NVec3f32.h"
 #include "lib/N/NType.h"
+#include "lib/N/Maths/NMath.h"
 // -------------------------------------------------------------------------------------------
 // NVec3Normalize
 // -------------------------------------------------------------------------------------------
@@ -22,19 +23,19 @@
 //		Vector length before normalization.
 //
 // -------------------------------------------------------------------------------------------
-Nf32 NVec3Normalize(NVEC3* v)
+Nf32 NVec3Normalize(NVEC3 *v)
 {
-	Nf32	n,sn;
+	Nf32 n, sn;
 
 	n = sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
-	if( n >= NF32_EPSILON_VECTOR_LENGTH )
+	if (n >= NF32_EPSILON_VECTOR_LENGTH)
 	{
 		sn = 1.0f / n;
 		v->x *= sn;
 		v->y *= sn;
 		v->z *= sn;
 	}
-	
+
 	return n;
 }
 
@@ -50,12 +51,12 @@ Nf32 NVec3Normalize(NVEC3* v)
 //		Vector length before normalization.
 //
 // -------------------------------------------------------------------------------------------
-Nf32 NVec3FastNormalize(NVEC3* v)
+Nf32 NVec3FastNormalize(NVEC3 *v)
 {
-	Nf32	n,sn;
+	Nf32 n, sn;
 
 	n = NFastSqrt(v->x * v->x + v->y * v->y + v->z * v->z);
-	if( n >= NF32_EPSILON_VECTOR_LENGTH )
+	if (n >= NF32_EPSILON_VECTOR_LENGTH)
 	{
 		sn = 1.0f / n;
 		v->x *= sn;
