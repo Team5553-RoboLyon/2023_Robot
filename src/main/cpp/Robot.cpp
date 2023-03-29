f15e0ddee13ae842c56eff6aa1731fada3a710e9
Nathan Mutin <32647745+nathanmutin@users.noreply.github.com>
2023-03-30 01:43:41 +0200

    avec le code autonomous

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,6 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "lib/NL/MotionControl/DriveTrain/Characterization/NLCharacterizationTable.h"
+
+#include "lib/NL/MotionControl/Trajectory/NLTrajectoryActionMessagesEnum.h"
 #include "Robot.h"
 
 // cc
@@ -63,7 +65,121 @@ void Robot::AutonomousInit()
   m_follower.initialize(&m_TrajectoryPack);
   m_state = Robot::STATE::PATH_FOLLOWING;
 }
-void Robot::AutonomousPeriodic() {}
+void Robot::AutonomousPeriodic() 
+{
+	NLRAMSETEOUTPUT output;
+	const NLFOLLOWER_TANK_OUTPUT* pout;
+	
+	NLTRJ_POSTED_MESSAGE message;	// Posted Message
+
+	switch (m_state)
+	{
+		case Robot::STATE::PATH_ERROR:
+			break;
+
+		case Robot::STATE::PATH_FOLLOWING:
+			// *****************************************************    'THE' METHOD(e)
+			// A) Feed back:
+			// avec les encodeurs on estime la position du robot:
+			//			l = distance parcourue par la roue gauche depuis le dernier reset encodeur.
+			//			r = distance parcourue par la roue droite depuis le dernier reset encodeur.
+			//
+			//			dl et dr = distances parcourues par les roues gauche et droite depuis le dernier call.
+			//			(note dl/dt = vitesse roue gauche et dr/dt = vitesse roue droite  )
+			//
+			/*
+			l = (m_leftGearboxEncoder.getRaw() / 8192.0f) * NF32_2PI * m_DriveTrainSpecs.m_wheelRadius;
+			r = (m_rightGearboxEncoder.getRaw() / 8192.0f) * NF32_2PI * m_DriveTrainSpecs.m_wheelRadius;
+			dl = l - m_dsLeftWheel;
+			dr = r - m_dsRightWheel;
+			m_dsLeftWheel = l;
+			m_dsRightWheel = r;
+			// forward:
+			m_estimatedPose.odometryUpdate(&m_DriveTrainSpecs, dl, dr, m_gyro.get());
+			*/
+			// backward:
+			//m_estimatedPose.odometryUpdate(&m_DriveTrainSpecs, -dr, -dl, m_gyro.get());
+			m_follower.estimate(m_leftGearboxEncoder.getRaw() / 8192.0f, m_rightGearboxEncoder.getRaw() / 8192.0f, m_gyro.get());
+
+			/*
+			// B) Feed forward : State ( full )
+			m_currrentPoint.m_kin.m_t += dt;
+			m_TrajectoryPack.getPoint(&m_currrentPoint, m_currrentPoint.m_kin.m_t);
+			*/
+			m_follower.updateTarget(&m_TrajectoryPack,dt);
+			// C) Ramsete:
+			//m_ramsete.update2(&output, &m_DriveTrainSpecs, &m_currrentPoint, &m_estimatedPose);
+			//m_ramsete.update3(&output, &m_follower);
+			
+			pout =  m_follower.compute();
+			// forward:
+			/*
+			m_moteurL1.SetVoltage(m_CrtzL1.getVoltage(output.m_leftVelocity, output.m_leftAcceleration));
+			m_moteurL2.SetVoltage(m_CrtzL2.getVoltage(output.m_leftVelocity, output.m_leftAcceleration));
+			m_moteurR1.SetVoltage(m_CrtzR1.getVoltage(output.m_rightVelocity, output.m_rightAcceleration));
+			m_moteurR2.SetVoltage(m_CrtzR2.getVoltage(output.m_rightVelocity, output.m_rightAcceleration));
+			*/
+			/*
+			// backward:
+			m_moteurR1.SetVoltage(m_CrtzL1.getVoltage(-output.m_leftVelocity, -output.m_leftAcceleration));
+			m_moteurR2.SetVoltage(m_CrtzL2.getVoltage(-output.m_leftVelocity, -output.m_leftAcceleration));
+
+			m_moteurL1.SetVoltage(m_CrtzR1.getVoltage(-output.m_rightVelocity,-output.m_rightAcceleration));
+			m_moteurL2.SetVoltage(m_CrtzR2.getVoltage(-output.m_rightVelocity,-output.m_rightAcceleration));
+			*/
+			m_moteurL1.SetVoltage(m_CrtzL1.getVoltage(pout->m_leftVelocity,		pout->m_leftAcceleration));
+			m_moteurL2.SetVoltage(m_CrtzL2.getVoltage(pout->m_leftVelocity,		pout->m_leftAcceleration));
+			m_moteurR1.SetVoltage(m_CrtzR1.getVoltage(pout->m_rightVelocity,	pout->m_rightAcceleration));
+			m_moteurR2.SetVoltage(m_CrtzR2.getVoltage(pout->m_rightVelocity,	pout->m_rightAcceleration));
+
+			//m_logCsv.write();
+
+			while (m_follower.getMessage(&message))
+			{
+				switch (message.m_id)
+				{
+					case OPEN_INTAKE:
+						m_allMechanisms.Command("open intake");
+						break;
+					case CLOSE_INTAKE:
+						m_allMechanisms.Command("close intake");
+						break;
+					case ACTIVATE_INTAKE_WHEELS:
+						m_allMechanisms.Command("activate intake wheels");
+						break;
+					case DEACTIVATE_INTAKE_WHEELS:
+						m_allMechanisms.Command("deactivate intake wheels");
+						break;
+					case ACTIVATE_CONVEYOR:
+						m_allMechanisms.Command("activate conveyor");
+						break;
+					case DEACTIVATE_CONVEYOR:
+						m_allMechanisms.Command("deactivate intake wheels");
+						break;
+
+					case ACTIVATE_AIM:
+						break;
+					case DEACTIVATE_AIM:
+						break;
+					case ACTIVATE_SHOOTER:
+						break;
+					case DEACTIVATE_SHOOTER:
+						break;
+
+					default:
+						break;
+				}
+			}
+
+			break;
+
+		case NLROBOT::STATE::PATH_END:
+			break;
+		default:
+			NErrorIf(1, NERROR_UNAUTHORIZED_CASE);
+			break;
+	}
+}
 
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {}
---
+++ src/main/include/lib/NL/MotionControl/Trajectory/NLTrajectoryActionMessagesEnum.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "lib/N/NCStandard.h"
+
+// Ce fichier est exclusivement utilis� par le code du Robot ( et simulateur robot ).
+// Il n'est pas utilis� par le logiciel d'�dition des chemins ( exception faite du code de simulation contenu dans NLRobot.cpp )
+// La liste des messages utilis�s par le logiciel d'�dition des chemins est issue du fichier : data/_actionmessages.txt
+// Ce fichier (_actionmessages.txt) est charg� et interpr�t� � chaque nouveau lancement du logiciel d'�dition des chemins.
+// Le programmeur du robot veillera � conserver la coh�rence entre l'enum ci-dessous et le contenu du fichier _actionmessages.txt
+// Les 2 d�crivent le m�me ensemble de messages.
+
+// ! Le premier message doit est �gal � 0 ! 
+// .. le suivant � 1, puis 2, etc ...
+typedef enum
+{
+	OPEN_INTAKE = 0,
+	CLOSE_INTAKE,
+	ACTIVATE_INTAKE_WHEELS,
+	DEACTIVATE_INTAKE_WHEELS,
+	ACTIVATE_CONVEYOR,
+	DEACTIVATE_CONVEYOR,
+	ACTIVATE_AIM,
+	DEACTIVATE_AIM,
+	ACTIVATE_SHOOTER,
+	DEACTIVATE_SHOOTER
+}NLACTIONMESSAGES_ENUM;
