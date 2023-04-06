b5bfcddf889d4f9ba2bfbf806fd4a23a2a031b86
mimi980 <90112270+mimi980@users.noreply.github.com>
2023-04-06 19:03:08 +0200

    take cube opti avec encoder

--- src/main/cpp/command/TakeInRobot/TakeCubeRobot.cpp
+++ src/main/cpp/command/TakeInRobot/TakeCubeRobot.cpp
@@ -36,6 +36,164 @@ void TakeCubeRobot::Execute()
   switch (m_State)
   {
   case State::High:
+    if (m_pArm->GetEncoder() < 0.0) // temps à réduire
+    {
+      m_pElevator->SetSetpoint(0.35); // valeur théorique à vérifier
+      m_count = 0;
+      m_State = State::Lowered;
+    }
+    break;
+  case State::Lowered:
+    if (m_pElevator->GetEncoder() < 5.0) // temps à réduire
+    {
+      m_pGripper->Close();
+      m_count = 0;
+      m_State = State::Taken;
+    }
+    break;
+  case State::Taken:
+    if (m_count > 50) // temps à réduire
+    {
+      m_pElevator->SetSetpoint(0.95);
+      m_count = 0;
+      m_State = State::GoDown;
+    }
+    break;
+  case State::GoDown:
+    if (m_pElevator->GetEncoder() > 90.0) // temps à réduire
+    {
+      m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+      m_count = 0;
+      m_State = State::Finish;
+    }
+    break;
+  case State::Finish:
+    if (m_pArm->GetEncoder() > NDEGtoRAD(80.0)) // temps à réduire
+    {
+      m_pElevator->SetSetpoint(0.0);
+    }
+    break;
+  default:
+    break;
+  };
+}
+
+// Called once the command ends or is interrupted.
+void TakeCubeRobot::End(bool interrupted)
+{
+  switch (m_State)
+  {
+  case State::High:
+    m_pElevator->SetSetpoint(0.80);
+    m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+    m_pElevator->IsWaiting = true;
+    break;
+  case State::Lowered:
+    m_pElevator->SetSetpoint(0.80);
+    m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+    m_pElevator->IsWaiting = true;
+    break;
+  case State::Taken:
+    m_pElevator->SetSetpoint(0.80);
+    m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+    m_pElevator->IsWaiting = true;
+    break;
+  case State::GoDown:
+    // retour défaut
+    break;
+  case State::Finish:
+    // retour défaut
+    break;
+  default:
+    break;
+  }
+}
+
+// Returns true when the command should end.
+bool TakeCubeRobot::IsFinished()
+{
+  // m_count = 0;
+  // m_count++;
+  // switch (m_State)
+  // {
+  // case (State::High):
+  //   m_pElevator->SetSetpoint(0.80);
+  //   if (m_count > 80)
+  //   {
+  //     m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+  //     return true;
+  //   }
+  //   break;
+  // case (State::Lowered):
+  //   m_pElevator->SetSetpoint(0.80);
+  //   if (m_count > 80)
+  //   {
+  //     m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+  //     return true;
+  //   }
+  //   break;
+  // case (State::Taken):
+  //   m_pElevator->SetSetpoint(0.80);
+  //   if (m_count > 80)
+  //   {
+  //     m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+  //     return true;
+  //   }
+  //   break;
+  // case (State::GoDown):
+  //   m_pArm->SetSetpoint(NDEGtoRAD(90.0));
+  //   if (m_count > 80)
+  //   {
+  //     return true;
+  //   }
+  //   break;
+
+  // default:
+  //   break;
+  // }
+  return false;
+}
+
+/*
+
+
+############################# CODE QUI MARCHE #############################
+
+
+#include "command/TakeInRobot/TakeCubeRobot.h"
+#include "lib/RblUtils.h"
+
+// ###################### A TESTER ######################
+
+TakeCubeRobot::TakeCubeRobot(Elevator *pElevator, Arm *pArm, Gripper *pGripper) : m_pElevator(pElevator), m_pArm(pArm), m_pGripper(pGripper)
+{
+  AddRequirements(m_pElevator);
+  AddRequirements(m_pArm);
+  AddRequirements(m_pGripper);
+}
+
+// Called when the command is initially scheduled.
+void TakeCubeRobot::Initialize()
+{
+  m_count = 0;
+  m_pElevator->SetSetpoint(0.80);
+  m_pGripper->Open();
+  m_pArm->SetSetpoint(NDEGtoRAD(-35.0)); // valeur théorique à vérifier
+  m_State = State::High;
+  // descend elevateur
+
+  // ferme pince
+
+  // remonte elevateur
+}
+
+// Called repeatedly when this Command is scheduled to run
+void TakeCubeRobot::Execute()
+{
+  m_count++;
+  switch (m_State)
+  {
+  case State::High:
     if (m_count > 80) // temps à réduire
     {
       m_pElevator->SetSetpoint(0.35); // valeur théorique à vérifier
@@ -153,3 +311,6 @@ bool TakeCubeRobot::IsFinished()
   // }
   return false;
 }
+
+
+*/
